a62064dda2be5ab334a75f69e4b643fae7dc288a
vpanuganti77 <vpanuganti13@gmail.com>
2025-10-16 12:46:37 +0530

    port changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.use(cors());
